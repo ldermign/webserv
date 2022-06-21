70668de62e79b94dda8d8e9586bd1299bb623ddb
Liena DERMIGNY <ldermign@e1r11p3.42.fr>
2022-06-21 13:58:00 +0200

    ldermign class begin

--- srcs/ConfigurationFile.hpp
+++ srcs/ConfigurationFile.hpp
@@ -6,11 +6,32 @@
 /*   By: ldermign <ldermign@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 15:35:12 by ldermign          #+#    #+#             */
-/*   Updated: 2022/06/20 15:37:17 by ldermign         ###   ########.fr       */
+/*   Updated: 2022/06/21 09:17:49 by ldermign         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONFIGURATIONFILE_HPP
 # define CONFIGURATIONFILE_HPP
+
+class ConfigurationFile {
+	
+public:
+
+//	ACCESSORS
+
+
+//	CANONICAL FORM
+
+	ConfigurationFile( void );
+	ConfigurationFile	&operator=( const ConfigurationFile &rhs );
+	ConfigurationFile( const ConfigurationFile &src );
+	virtual	~ConfigurationFile( void );
+
+
+private:
+
+
+	
+};
 
 #endif
--- srcs/parsing/ConfigurationFile.cpp
+++ srcs/parsing/ConfigurationFile.cpp
@@ -6,8 +6,30 @@
 /*   By: ldermign <ldermign@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 14:22:32 by ldermign          #+#    #+#             */
-/*   Updated: 2022/06/20 15:35:04 by ldermign         ###   ########.fr       */
+/*   Updated: 2022/06/21 09:21:14 by ldermign         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ConfigurationFile.hpp"
+
+/*
+**	CANONICAL FORM
+*/
+
+ConfigurationFile::ConfigurationFile( void ) {}
+
+ConfigurationFile	&ConfigurationFile::operator=( const ConfigurationFile &rhs ) {
+
+	if (this != &rhs) {
+		
+	}
+	
+	return *this;
+}
+
+ConfigurationFile::ConfigurationFile( const ConfigurationFile &src ) {
+	
+	*this = src;
+}
+
+ConfigurationFile::~ConfigurationFile( void ) {}
